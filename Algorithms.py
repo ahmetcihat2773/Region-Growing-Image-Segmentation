e13591d614a5e639b198b9b6831419b6c0dc4584
Ahmet Cihat Bozkurt <ahmetcihat2773@gmail.com>
2021-01-01 15:14:43 +0100

    Region growing with local intentsy

--- Algorithms.py
+++ Algorithms.py
@@ -11,14 +11,18 @@ class Algorithms():
         plt.title(title)
         plt.show()
     def apply_otsu(self,img):
-        ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        ret2,th2 = cv2.threshold(img,0,255,cv2.THRESH_OTSU)
         return th2
-    def apply_dilation(self,img,iterat):
+    def apply_dilation(self,img,kernel_size,iterat):
         # Apply dilation : 
-        kernel = np.ones((3,3),np.uint8)
+        kernel = np.ones((kernel_size,kernel_size),np.uint8)
         dilation = cv2.dilate(img,kernel,iterations = iterat)
         return dilation
-    def region_growing(self,thrs_image):
+    def region_growing_binary(self,thrs_image):
+        """
+        This algorithm has problem with the labeling unconnected pixels.
+
+        """
         """
         thrs_image is the thresholded image whose value is 0 or 255.
 
@@ -27,7 +31,9 @@ class Algorithms():
         self.thrs_image = thrs_image
         row_num = thrs_image.shape[0]
         col_num = thrs_image.shape[1]
-        label = 1
+        # create emtpy image
+        self.label_image = np.zeros((row_num,col_num),dtype=int)
+        label = 254
         self.value = 255
         self.stack = list()
         self.stack_empty = True
@@ -43,9 +49,32 @@ class Algorithms():
                         self.label_check(cr_i,cr_j,label)
                     else:
                         self.stack_empty = True
-                        label = label +20
-                        print("LABEL",label)
+                        label = label -5
+                        print("LABEL LABEL")
         return self.thrs_image
+    def label_check_comp(self,i,j,label):
+        self.label_image[i,j] = label 
+        gray_sum_avg = 0
+        # Take the kernel average
+        for r in range(i-1,i+2,1):
+            for e in range(j-1,j+2,1):
+                gray_sum_avg += self.thrs_image[r,e]
+        gray_sum_avg = gray_sum_avg/9
+        #print("(",i,j,")")
+        #print(gray_sum_avg,"::",int(255/2))
+        for r in range(i-1,i+2,1):
+            for e in range(j-1,j+2,1):
+                if self.thrs_image[r,e] == self.value:
+                    if r == i and e == j:
+                        continue
+                    else:                            
+                        if not [r,e] in self.stack:
+                            self.stack.append([r,e])  
+                if int(gray_sum_avg) >= int(255/2):
+                    self.label_image[r,e] = 255
+                               
+        
+        self.stack_empty = False
     def label_check(self,i,j,label):
         self.thrs_image[i,j] = label
         for r in range(i-1,i+2,1):
--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import cv2
 from Algorithms import Algorithms 
 from skimage.measure import label
 from matplotlib import pyplot as plt 
-
-img = cv2.imread('./UTFVP/0053_1_1_120511-103029.png',cv2.CV_8U)
+img = cv2.imread('./FV-USM/01 (4).jpg',cv2.CV_8U)
 print(img.shape)
 algorithms = Algorithms()
+# BINARY REGION GROWING
 #algorithms.showImage(img,"Original Image")
-
-otsu_img = algorithms.apply_otsu(img)
-algorithms.showImage(otsu_img,"After OTSU Thresholding")
-#dila_img = algorithms.apply_dilation(otsu_img,40)
+#otsu_img = algorithms.apply_otsu(img)
+#algorithms.showImage(otsu_img,"After OTSU Thresholding")
+#dila_img = algorithms.apply_dilation(otsu_img,3,10)
+#dila_img_1 = algorithms.apply_dilation(dila_img,3,10)
 #algorithms.showImage(dila_img,"After Dilation")
-region_growing = algorithms.region_growing(otsu_img)
-algorithms.showImage(region_growing,"After Region Growing")
+#region_growing = algorithms.region_growing_binary(dila_img)
+#algorithms.showImage(region_growing,"After Region Growing")
+#imC = cv2.applyColorMap(region_growing, cv2.COLORMAP_JET)
+#algorithms.showImage(imC,"Color Map")
 
-
+# GRAY LEVEL REGION GROWING
 
  
